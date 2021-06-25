82bb28d83d7d35a0cf07552196391101cc3ed1d6
t.yoshizawa <t.yoshizawa@172.16.102.73>
2021-06-25 17:01:57 +0900

    First Commit

---
+++ src/test/java/jp/ac/jc21/fegogo2/Fegogo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.jc21.fegogo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fegogo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
